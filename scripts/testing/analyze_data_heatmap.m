312318ad821e28e92c15194060ba4b08e740ca4f
Sam Freitas <60946332+Sam-Freitas@users.noreply.github.com>
2021-09-09 17:06:25 -0700

    analysis package

--- scripts/testing/analyze_data_heatmap.m
+++ scripts/testing/analyze_data_heatmap.m
@@ -2,6 +2,17 @@ clear all
 clc
 close all force hidden
 
+csv_output_header = ["Biological Replicate","Condition","ID (well location)",...
+    "Is Dead","Is Last Day Censored","Last Day of Observation","First Day of nonzero data",...
+    ...
+    "Intensity at First Day of Infection","Intensity at Last Day of Observation",...
+    "Intensity Regression Slope","Intensity Regresstion intercept",...
+    "Intensity Integrated Across Time","Max Intensity Slope to a Point"...
+    ...
+    "Area at First Day of Infection","Area at Last Day of Observation",...
+    "Area Regression Slope","Area Regresstion intercept",...
+    "Area Integrated Across Time","Max Area Slope to a Point"];
+
 % get csv
 [CSV_filename,CSV_filepath] = uigetfile('/Volumes/Sutphin server/Users/Luis Espejo/SICKO/Experiments/*.csv',...
     'Select the Compiled csv File');
@@ -87,6 +98,11 @@ non_cen_data_area = data_area.*idx_2d_data_to_keep;
 non_cen_data_area(idx_2d_data_to_keep==-1) = -1;
 non_cen_data_intensity = data_intensity.*idx_2d_data_to_keep;
 non_cen_data_intensity(idx_2d_data_to_keep==-1) = -1;
+
+data_to_csv(csv_output_header,csv_table,...
+    CSV_filename,CSV_filepath,...
+    data_intensity,data_area,data_censor,data_dead,...
+    data_sess_died,non_cen_data_area,non_cen_data_intensity);
 
 display_data(non_cen_data_area,logical(idx_infected.*(~idx_only_single_point)),...
     conditions,csv_table,data_censor,'Integrated_Area')
@@ -203,3 +219,155 @@ for i = 1:length(conditions)
 end
 
 end
+
+
+
+function data_to_csv(csv_output_header,csv_table,...
+    CSV_filename,CSV_filepath,...
+    data_intensity,data_area,data_censor,data_dead,...
+    data_sess_died,non_cen_data_area,non_cen_data_intensity)
+
+% get names of the csv
+csv_names = csv_table.Properties.VariableNames;
+
+% get first three headers 
+col_biorep = contains(string(csv_names),"Biological Replicate");
+col_condition = contains(string(csv_names),"Condition");
+col_location = contains(string(csv_names),"ID (well location)");
+
+% get all the data as a cell array
+data_cells = table2cell(csv_table);
+
+% get the bioreps 
+bioreps = data_cells(:,col_biorep);
+conditions = data_cells(:,col_condition);
+locations = data_cells(:,col_location);
+
+% inialize the data 
+intensity_regression_intercept = zeros(size(data_sess_died));
+intensity_regression_slope = zeros(size(data_sess_died));
+intensity_integrated_across_time = zeros(size(data_sess_died));
+intensity_max_gradient_at_point = zeros(size(data_sess_died));
+first_sess_of_infection_intensity = nan(size(data_sess_died));
+last_sess_of_infection_intensity = nan(size(data_sess_died));
+
+area_regression_slope = zeros(size(data_sess_died));
+area_regression_intercept = zeros(size(data_sess_died));
+area_integrated_across_time = zeros(size(data_sess_died));
+area_max_gradient_at_point = zeros(size(data_sess_died));
+first_sess_of_infection_area = nan(size(data_sess_died));
+last_sess_of_infection_area = nan(size(data_sess_died));
+
+final_data_censor = zeros(size(data_sess_died));
+last_day_of_observation = nan(size(data_sess_died));
+first_sess_nonzero_data = nan(size(data_sess_died));
+
+
+%iterate through each animal
+for i = 1:length(data_sess_died)
+    % find the data for intesity or area doesnt really matter for this
+    % animal in the data
+    this_data_inten = data_intensity(i,:);
+    this_data_area = data_area(i,:);
+    x = 1:length(this_data_inten);
+    
+    % try to find the first session that it was infected
+    this_first_sess_idx = find((this_data_inten>0) == 1, 1, 'first');
+    % if there is a death detected find the last day of observation 
+    % that would be the day of death -1
+    this_last_sess_idx = data_sess_died(i) - 1;
+    % if the value of death day is negative then it is still alive and then
+    % so the length of the experiment is the last observed day
+    this_last_sess_idx(this_last_sess_idx<0) = size(data_area,2);
+    
+    % if there is a session with nonzeros isolate the specifc data
+    if ~isempty(this_first_sess_idx)
+        first_sess_nonzero_data(i) = this_first_sess_idx;
+        
+        first_sess_of_infection_intensity(i) = data_intensity(i,this_first_sess_idx);
+        last_sess_of_infection_intensity(i) = data_intensity(i,this_last_sess_idx);
+        
+        first_sess_of_infection_area(i) = data_area(i,this_first_sess_idx);
+        last_sess_of_infection_area(i) = data_area(i,this_last_sess_idx);
+        
+        this_linear_regression_inten = polyfit(x,this_data_inten,1);
+        intensity_regression_slope(i) = this_linear_regression_inten(1);
+        intensity_regression_intercept(i) = this_linear_regression_inten(2);
+        
+        this_linear_regression_area = polyfit(x,this_data_area,1);
+        area_regression_slope(i) = this_linear_regression_area(1);
+        area_regression_intercept(i) = this_linear_regression_area(2);
+        
+        intensity_integrated_across_time(i) = sum(this_data_inten,'omitnan');
+        area_integrated_across_time(i) = sum(this_data_area,'omitnan');
+        
+        intensity_max_gradient_at_point(i) = max(gradient(this_data_inten));
+        area_max_gradient_at_point(i) = max(gradient(this_data_area));
+        
+    end
+    
+    last_day_of_observation(i) = this_last_sess_idx;
+    
+    if isnan(data_censor(i,end))
+        cen_idx = find(~isnan(data_censor(i,:)) == 1,1,'last');
+        final_data_censor(i) = data_censor(i,cen_idx);
+    else
+        final_data_censor(i) = data_censor(i,end);
+    end
+    
+end
+
+
+
+final_array = cell(size(data_area,1),length(csv_output_header));
+
+for i = 1:size(data_area,1)
+    
+    final_array{i,1} = bioreps{i};
+    final_array{i,2} = conditions{i};
+    final_array{i,3} = locations{i};
+    
+    final_array{i,4} = logical(data_sess_died(i));
+    final_array{i,5} = logical(final_data_censor(i));
+    final_array{i,6} = last_day_of_observation(i);
+    final_array{i,7} = first_sess_nonzero_data(i);
+    
+    final_array{i,8}  = first_sess_of_infection_intensity(i);    
+    final_array{i,9}  = last_sess_of_infection_intensity(i);  
+    final_array{i,10} = intensity_regression_slope(i);  
+    final_array{i,11} = intensity_regression_intercept(i); 
+    final_array{i,12} = intensity_integrated_across_time(i);  
+    final_array{i,13} = intensity_max_gradient_at_point(i); 
+    
+    final_array{i,14} = first_sess_of_infection_area(i);    
+    final_array{i,15} = last_sess_of_infection_area(i);  
+    final_array{i,16} = area_regression_slope(i);  
+    final_array{i,17} = area_regression_intercept(i); 
+    final_array{i,18} = area_integrated_across_time(i);  
+    final_array{i,19} = area_max_gradient_at_point(i); 
+    
+end
+
+[~,out_name,~] = fileparts(char(CSV_filename));
+
+output_name = [char(out_name) '_analyzed.csv'];
+
+output_path = fullfile(CSV_filepath,output_name);
+
+disp(output_path);
+
+T = cell2table(final_array,'VariableNames',csv_output_header);
+
+writetable(T,output_path);
+
+
+
+end
+
+
+
+
+
+
+
+
