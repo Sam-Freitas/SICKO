fb054a33aadf1ab0065bf0da73344dfc728a4cfb
Sam Freitas <60946332+Sam-Freitas@users.noreply.github.com>
2022-09-23 12:39:40 -0700

    single worm grayscale image production

--- scripts/testing/Get_single_worm_progression.m
+++ scripts/testing/Get_single_worm_progression.m
@@ -2,13 +2,13 @@ clear all
 curr_path = pwd;
 
 % WORM_TO_ISOLATE
-worm_condition = 'SU10_EV';
-worm_location = '2g';
+worm_condition = "SU10_EV";
+worm_location = ["2g", "2e", "1d", "3h"];
 img_thresh = 150;
 
 data_path = fullfile('/Volumes/Sutphin server/Users/Luis Espejo/SICKO/Experiments/');
 
-overarching_folder = 'Y:\Users\Luis Espejo\SICKO\Experiments\Test Dual Validation - Copy';
+% overarching_folder = 'Y:\Users\Luis Espejo\SICKO\Experiments\Test Dual Validation - Copy';
 overarching_folder = '/Volumes/Sutphin server/Users/Luis Espejo/Terasaki Validation SU10';
 
 [final_save_path,final_save_name,~] = fileparts(overarching_folder);
@@ -82,6 +82,7 @@ for n = 1:length(ovr_dir)
     clear condition2
     
     % mainFolder = uigetdir();    % Selectyour Main folder
+    disp('Reading all file attributes (might take a while)')
     if ispc
         [~,message,~] = fileattrib([fullfile(ovr_dir(n).folder,ovr_dir(n).name),'\*']);
     else
@@ -101,100 +102,72 @@ for n = 1:length(ovr_dir)
     TIF_filepaths = TIF_filepaths(CSV_flag == 1);
     
     fprintf('There are %i files with *.TIF exts.\n',numel(TIF_filepaths));
-    
-    single_worm_idx = contains(string(TIF_filepaths),worm_location).*contains(string(TIF_filepaths),worm_condition);
-    
-    fprintf('There are %i files for this single worm.\n',sum(single_worm_idx));
-    
-    single_worm_filepaths = TIF_filepaths(logical(single_worm_idx))';
-    
-    se = strel('disk',3);
 
-    for i = 1:length(single_worm_filepaths)
-        temp = single_worm_filepaths{i};
-        temp2{i} = temp(find(temp=='D',1,'last')+1:find(temp=='/',1,'last')-1);
-    end
-    [days_to_sort,ndx,dbg] = natsort(temp2');
-    days_to_sort = str2double(string(days_to_sort));
-    clear temp2
-    single_worm_filepaths = single_worm_filepaths(ndx);
-    
-    count = 1;
-    imgs = cell(1,length(single_worm_filepaths)/3);
-    for i = 1:3:length(single_worm_filepaths)
-        disp(i)
-        imgs{count} = double(imread(single_worm_filepaths{i}));
-        masks{count} = bwareafilt(imclose(bwareaopen(imgs{count}>img_thresh,10,4),se),1);
-        count = count + 1;
-    end
-    
-    unique_days_to_sort = sort(unique(days_to_sort),'ascend');
-    overall_max = 0;
-    for i = 1:length(imgs)
-        if overall_max < max(imgs{i}(:))
-            overall_max = max(imgs{i}(:));
-        end
-    end
+    for j = 1:length(worm_location)
 
-    overall_max = 750;
+        this_worm_location = char(worm_location(j));
 
-    for i = 1:length(imgs)
-        temp_img = imgs{i}/overall_max;
-        imgs2{i} = insertText(temp_img,[1,1],...
-            ['Day ' num2str(unique_days_to_sort(i))],'FontSize', 100);
-    end
+        this_worm_condition = char(worm_condition);
+
+        disp([this_worm_condition '_' this_worm_location])
         
-    tiled_img = imtile(imgs2);
-    tiled_mask = imtile(masks);
-%     
-%     out1 = imfuse(tiled_img,tiled_mask);    
-%     imshow(out1)
-%     
-%     d = 250;
-%     for i = 1:length(imgs)
-%         s = regionprops(masks{i},'Centroid');
-%         cent{i} = round([s.Centroid(2),s.Centroid(1)]);
-%         
-%         cropx = [cent{i}(1)-d,cent{i}(1)+d];
-%         cropy = [cent{i}(2)-d,cent{i}(2)+d];
-%         
-%         if any(cropy < 0)
-%             cropy = cropy - min(cropy) + 1;
-%         end
-%         if any(cropx < 0)
-%             cropx = cropx - min(cropx) + 1;
-%         end
-%         
-%         img_size = size(masks{1},1);
-%         if any(cropy > img_size)
-%             cropy = cropy - (max(cropy)-img_size);
-%         end
-%         if any(cropx > img_size)
-%             cropx = cropx - (max(cropx)-img_size);
-%         end
-%         
-%         imgs3{i} = imgs{i}(cropx(1) : cropx(2),cropy(1) : cropy(2)); 
-%         masks2{i} = masks{i}(cropx(1) : cropx(2),cropy(1) : cropy(2)); 
-%     end
-%     
-%     first_mean = mean2(imgs3{1});
-%     imgs3{1} = insertText(imgs3{1}/max(imgs3{1}(:)),[100,100],['Day ' num2str(1)],'FontSize', 48);
-%     for i = 2:length(imgs)
-%         scaler = mean2(imgs3{i})/first_mean;
-%         imgs3{i} = imgs3{i}/scaler;
-%         imgs3{i} = insertText(imgs3{i}/max(imgs3{i}(:)),[100,100],['Day ' num2str(i)],'FontSize', 48);
-%         disp(mean2(imgs3{i}))
-%     end
-%     
-%     
-%     tiled_img = imtile(imgs3,'GridSize',[2,3]);
-%     tiled_mask = imtile(masks2,'GridSize',[2,3]);
+        single_worm_idx = contains(string(TIF_filepaths),this_worm_location).*...
+            contains(string(TIF_filepaths),this_worm_condition);
+        
+        fprintf('There are %i files for this single worm.\n',sum(single_worm_idx));
+        
+        single_worm_filepaths = TIF_filepaths(logical(single_worm_idx))';
+        
+        se = strel('disk',3);
     
-    figure;
-    out2 = imfuse(tiled_img,tiled_mask/2,'Scaling','joint');    
-    imshow(out2)
+        for i = 1:length(single_worm_filepaths)
+            temp = single_worm_filepaths{i};
+            temp2{i} = temp(find(temp=='D',1,'last')+1:find(temp=='/',1,'last')-1);
+        end
+        [days_to_sort,ndx,dbg] = natsort(temp2');
+        days_to_sort = str2double(string(days_to_sort));
+        clear temp2
+        single_worm_filepaths = single_worm_filepaths(ndx);
+        
+        count = 1;
+        imgs = cell(1,length(single_worm_filepaths)/3);
+        masks = cell(1,length(single_worm_filepaths)/3);
+        disp('Reading in images')
+        for i = 1:3:length(single_worm_filepaths)
+            imgs{count} = double(imread(single_worm_filepaths{i}));
+%             masks{count} = bwareafilt(imclose(bwareaopen(imgs{count}>img_thresh,10,4),se),1);
+            count = count + 1;
+        end
+        
+        unique_days_to_sort = sort(unique(days_to_sort),'ascend');
+        overall_max = 0;
+        for i = 1:length(imgs)
+            if overall_max < max(imgs{i}(:))
+                overall_max = max(imgs{i}(:));
+            end
+        end
     
-    imwrite(out2,'out2.png')
+        overall_max = 750;
+        
+        imgs2 = cell(size(imgs));
+        for i = 1:length(imgs)
+            temp_img = imgs{i}/overall_max;
+            imgs2{i} = insertText(temp_img,[1,1],...
+                ['Day ' num2str(unique_days_to_sort(i))],'FontSize', 100);
+        end
+            
+        tiled_img = imtile(imgs2);
+%         tiled_mask = imtile(masks);
+% 
+%         figure;
+%         out2 = imfuse(tiled_img,tiled_mask/2,'Scaling','joint');    
+%         imshow(out2)
+    
+        file_name = [this_worm_condition '_' this_worm_location '_out2.jpg'];
+        
+        imwrite(tiled_img,file_name)
+        disp(file_name)
+    end
 %     imwrite(out2,'out2.png')
     
 end
@@ -213,5 +186,48 @@ end
 %     writetable(T,fullfile(data_path,[final_save_name '_1.csv']))
 % end
 
+    %     
+    %     out1 = imfuse(tiled_img,tiled_mask);    
+    %     imshow(out1)
+    %     
+    %     d = 250;
+    %     for i = 1:length(imgs)
+    %         s = regionprops(masks{i},'Centroid');
+    %         cent{i} = round([s.Centroid(2),s.Centroid(1)]);
+    %         
+    %         cropx = [cent{i}(1)-d,cent{i}(1)+d];
+    %         cropy = [cent{i}(2)-d,cent{i}(2)+d];
+    %         
+    %         if any(cropy < 0)
+    %             cropy = cropy - min(cropy) + 1;
+    %         end
+    %         if any(cropx < 0)
+    %             cropx = cropx - min(cropx) + 1;
+    %         end
+    %         
+    %         img_size = size(masks{1},1);
+    %         if any(cropy > img_size)
+    %             cropy = cropy - (max(cropy)-img_size);
+    %         end
+    %         if any(cropx > img_size)
+    %             cropx = cropx - (max(cropx)-img_size);
+    %         end
+    %         
+    %         imgs3{i} = imgs{i}(cropx(1) : cropx(2),cropy(1) : cropy(2)); 
+    %         masks2{i} = masks{i}(cropx(1) : cropx(2),cropy(1) : cropy(2)); 
+    %     end
+    %     
+    %     first_mean = mean2(imgs3{1});
+    %     imgs3{1} = insertText(imgs3{1}/max(imgs3{1}(:)),[100,100],['Day ' num2str(1)],'FontSize', 48);
+    %     for i = 2:length(imgs)
+    %         scaler = mean2(imgs3{i})/first_mean;
+    %         imgs3{i} = imgs3{i}/scaler;
+    %         imgs3{i} = insertText(imgs3{i}/max(imgs3{i}(:)),[100,100],['Day ' num2str(i)],'FontSize', 48);
+    %         disp(mean2(imgs3{i}))
+    %     end
+    %     
+    %     
+    %     tiled_img = imtile(imgs3,'GridSize',[2,3]);
+    %     tiled_mask = imtile(masks2,'GridSize',[2,3]);
 
 
